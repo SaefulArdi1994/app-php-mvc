dbd32f9d89893987c54c9e6e932dd1201f7c16b6
Saeful Ardi <saeful.4rdi@gmail.com>
2024-11-11 00:35:15 +0700

    update routing

--- app/core/App.php
+++ app/core/App.php
@@ -5,6 +5,25 @@ class App {
     function __construct( )
     {
         // echo "Test APP";
+        $url = $this->parseUrl();
+        var_dump($url);
+    }
+
+    public function parseUrl()
+    {
+        // Jika ada url yang dikirimkan
+        if(isset($_GET['url'])) {
+
+            // ambil url = isi dengan url nya
+            
+            $url = rtrim($_GET['url'], '/'); // rtrim untuk menghapus '/'
+            $url = filter_var($url, FILTER_SANITIZE_URL); // membersihkan url dari karakter yang aneh
+            $url = explode('/', $url); // memecahkan url dengan delimiter '/'
+
+            //kembalikan nilai url
+            return $url;
+        }
+
     }
 
 }
